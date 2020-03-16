b2e584e9b1c6f29862fb34837cf3c3ba0c4d389e
Nuutti <nuutti.heinasmaki@hotmail.fi>
2020-03-16 16:03:25 +0200

    Add initial project files
    Update .gitignore
    update

---
+++ Alchemist Escape Room Game/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour{
+    public static GameMaster Instance;
+    public Vector3 startLocation; // = (0, 0, 0); expect player to spawn here
+
+    void Awake(){
+        Instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
---
+++ Alchemist Escape Room Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour{
+    public Vector3 targetPosition = GameMaster.Instance.startLocation;
+    private float camHeight = GameMaster.Instance.startLocation.y; // 0
+
+    // Locations for the rooms left and rightmost positions on X
+    public float leftWallX;
+    public float rightWallX;
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Mouse0)){
+            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPosition.y = camHeight;
+            if(targetPosition.x < leftWallX) targetPosition.x = leftWallX;
+            if(targetPosition.x > rightWallX) targetPosition.x = rightWallX;
+        }
+ 
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * 5);
+    }
+}
