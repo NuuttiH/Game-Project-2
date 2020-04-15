f47f0eeff2c1fadd2516bc1b85d32983de518b3c
Nuutti <nuutti.heinasmaki@hotmail.fi>
2020-04-15 12:35:49 +0300

    Assign spritesheet and animate player

--- Alchemist Escape Room Game/Assets/Scripts/PlayerController.cs
+++ Alchemist Escape Room Game/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour{
     public static PlayerController Instance;
+    public SpriteRenderer spriteRenderer;
+    public Animator animator;
     public Vector3 targetPosition;
 
     public float objectActivationDistance = 3.8f;
@@ -34,6 +36,10 @@ public class PlayerController : MonoBehaviour{
         if(!EventSystem.current.IsPointerOverGameObject() && !GameMaster.Instance.menuOpen
         && GameMaster.Instance.puzzleOpen==0 && Input.GetKeyDown(KeyCode.Mouse0)){
             queuedAction = null;
+
+            if(cameraMain.ScreenToWorldPoint(Input.mousePosition).x < transform.position.x) spriteRenderer.flipX = false;
+            else spriteRenderer.flipX = true;
+
             // Interactive action
             bool didAction = false;
             if(mouseOverInteractiveObject != null){
@@ -72,6 +78,7 @@ public class PlayerController : MonoBehaviour{
             }
             // Move if no interactive action
             if(!didAction){
+                animator.SetBool("IsWalking", true);
                 targetPosition = cameraMain.ScreenToWorldPoint(Input.mousePosition);
                 targetPosition.y = GameMaster.Instance.camHeight;
                 if(targetPosition.x < leftWall) targetPosition.x = leftWall;
@@ -82,30 +89,34 @@ public class PlayerController : MonoBehaviour{
                 targetPosition = transform.position;
             }
         }
-        else if(queuedAction!=null && transform.position==targetPosition){
-            // Do queued action
-            queuedAction.DoDialogue();
-            if(queuedAction.pickupOnAction){ 
-                GameMaster.Instance.PickupItem(queuedAction.item);
-                Destroy(queuedAction.gameObject);
-            }
-            else if(queuedAction.puzzleCombine1!=null){
-                PuzzleCombine1Controller.Instance.OpenPuzzle(
-                queuedAction.puzzleCombine1);
-            }
-            else if(queuedAction.puzzleCombine2!=null){
-                PuzzleCombine2Controller.Instance.OpenPuzzle(
-                queuedAction.puzzleCombine2);
-            }
-            else if(queuedAction.combinationLock!=null){
-                CombinationLockController.Instance.OpenPuzzle(
-                queuedAction.combinationLock);
-            }
-            else if(queuedAction.puzzleSpellbook!=null){
-                SpellbookController.Instance.OpenPuzzle(
-                queuedAction.puzzleSpellbook);
-            }
-            queuedAction = null;
+        else if(transform.position==targetPosition){
+            animator.SetBool("IsWalking", false);
+
+            if(queuedAction!=null){
+                // Do queued action
+                queuedAction.DoDialogue();
+                if(queuedAction.pickupOnAction){ 
+                    GameMaster.Instance.PickupItem(queuedAction.item);
+                    Destroy(queuedAction.gameObject);
+                }
+                else if(queuedAction.puzzleCombine1!=null){
+                    PuzzleCombine1Controller.Instance.OpenPuzzle(
+                    queuedAction.puzzleCombine1);
+                }
+                else if(queuedAction.puzzleCombine2!=null){
+                    PuzzleCombine2Controller.Instance.OpenPuzzle(
+                    queuedAction.puzzleCombine2);
+                }
+                else if(queuedAction.combinationLock!=null){
+                    CombinationLockController.Instance.OpenPuzzle(
+                    queuedAction.combinationLock);
+                }
+                else if(queuedAction.puzzleSpellbook!=null){
+                    SpellbookController.Instance.OpenPuzzle(
+                    queuedAction.puzzleSpellbook);
+                }
+                queuedAction = null;
+            } 
         }
             
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * 5);
